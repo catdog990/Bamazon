6e6108262985f0967916960205ffd0b2cacc3702
catdog990 <ozmcastro@gmail.com>
2018-01-15 23:44:26 -0500

    things have changed

--- customerView.js
+++ customerView.js
@@ -30,65 +30,76 @@ itemList();
 // **********could make this prompt into a list, and use "switch."***///
 function askID(){
 	inquirer
-		.prompt({
-			name:"action",
+		.prompt([
+		{	name:"action",
 			type:"input",
 			message:"Enter the ID of the item you would like to purchase."
-		})
+		},
+		{	name:"answer",
+			type:"input",
+			message:"How many would you like to buy?"
+
+		}
+		])
 		.then(function(response){
 
+			var itemID = response.action;
+			var purch = response.answer;
+
+			var update = res[0].in_stock - purch;
+			connection.query("UPDATE items SET ? WHERE ?",
+			[{
+				in_stock:
+			}]
+			// let sql = "UPDATE items SET in_stock = in_stock - purch WHERE id = itemID";
+				connection.query(sql, (err, res) =>{
+					if(err) throw err;
+						itemList();
+						
+				});
+
 			
 
 			
-			//***Run casper function***//
-			// if(response.action){
-			// 	;
-			// 	var query = "SELECT id, Name, Department, Price, In-Stock FROM bamazon WHERE?";
-			// 	connection.query(query, {action: response.action }, function(err, res){
-			// 		for (var i = 0; i < res.length; i++){
-			// 			console.log("ID: " + res[i].id); 
 
-			// 			// + "|| Department: " + res[i].Department + "|| Price: " + res[i].Price);
-			// 		};
-			// 	});
-			// };
-			purchase();
+			
+
 
 			});
-		var idRes =response.action;
+		
 
 		};
 
 
-function purchase(){
-	inquirer
-	.prompt({
-		name:"action",
-		type:"input",
-		message:"How many would you like to buy?"
-	})
-		.then(function(response){
-			if(response.action){
+// function purchase(){
+// 	inquirer
+// 	.prompt({
+// 		name:"action",
+// 		type:"input",
+// 		message:"How many would you like to buy?"
+// 	})
+// 		.then(function(response){
+// 			if(response.action){
 			
 
-			// connection.query(sql, (err, res) =>{
-			// 	if (err) throw err;
+// 			connection.query(sql, (err, res) =>{
+// 				if (err) throw err;
 
-			// 	var sql = "UPDATE items SET in_stock = in_stock - response.action WHERE id = askID.idRes"
+// 				var sql = "UPDATE items SET in_stock = in_stock - response.action WHERE id = askID.idRes"
 
-			// 	connection.query(sql, (err, res) =>{
-			// 		if(err) throw err;
-			// 		askID();
-			// 	});
-			// });
+// 			// 	connection.query(sql, (err, res) =>{
+// 			// 		if(err) throw err;
+// 			// 		askID();
+// 			// 	});
+// 			// });
 			
-			console.log(askID.idRes);
+// 			console.log(askID.inquirer.then.idRes);
 
-			}
+// 			}
 
-			// console.log("You've purchased " + response.action +"!")
-		})
+// 			// console.log("You've purchased " + response.action +"!")
+// 		})
 		
-};
+// };
 
 
